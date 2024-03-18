4bb4dbe1ddbffa267a16714aee0131ca9be28f01
khalidzahra <khalidzahra923@gmail.com>
2024-03-19 00:08:52 +0200

    Add struct for parsing contract response

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -4,3 +4,22 @@ type Extractor interface {
 	loadAPIKey()
 	findContractSource(string) string
 }
+
+type ContractSourceResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Result  []struct {
+		SourceCode       string `json:"SourceCode"`
+		ABI              string `json:"ABI"`
+		ContractName     string `json:"ContractName"`
+		CompilerVersion  string `json:"CompilerVersion"`
+		OptimizationUsed string `json:"OptimizationUsed"`
+		Runs             string `json:"Runs"`
+		ConstructorArgs  string `json:"ConstructorArguments"`
+		EVMVersion       string `json:"EVMVersion"`
+		Library          string `json:"Library"`
+		LicenseType      string `json:"LicenseType"`
+		Proxy            string `json:"Proxy"`
+		Implementation   string `json:"Implementation"`
+	} `json:"result"`
+}
